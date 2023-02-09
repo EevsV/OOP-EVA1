c94f430e1f14a609cc1b67950a834c5f01a4116b
EevsV <124392589+EevsV@users.noreply.github.com>
2023-02-09 12:55:22 -0600

    Add files via upload

---
+++ OOP-EVA1-main/EVA1_2_CLASES_JAVA/src/eva1_2_clases_java/EVA1_2_CLASES_JAVA.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_2_clases_java;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_CLASES_JAVA {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // CREAR OBJETO
+        // INSTANCIACION
+        // INSTANCIAR --> DAR MEMORIA RAM
+        // NOMBRE DE LA CLASE IDENTIFICADOR = new CONSTRUCTOR DE LA CLASE
+        Persona perso1 = new Persona();
+        
+        /* perso1.apellidos = "Gardea Saucedo";
+        perso1.nombre = "Sebastian";
+        System.out.println(perso1.nombre + " " + perso1.apellidos);
+                */
+     
+        
+       perso1.setNombre("Sebastian");
+       perso1.setApellidos("Gardea Saucedo");
+       perso1.setEdad(18);
+       perso1.setGenero('M');
+        /* System.out.println(perso1.getNombre() + " " + perso1.getApellidos());
+        System.out.println(perso1.getEdad());
+        System.out.println(perso1.getGenero()); */
+       perso1.imprimirDatos();
+    }
+    
+}
