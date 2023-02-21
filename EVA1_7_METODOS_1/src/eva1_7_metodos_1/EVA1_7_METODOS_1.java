1fc05768acf40f338d3f4f407bb204c90fbd49da
EevsV <124392589+EevsV@users.noreply.github.com>
2023-02-21 12:41:56 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_7_metodos_1;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_7_METODOS_1 {
+
+ 
+    public static void main(String[] args) {
+        myMethod();
+    }
+    
+    public static void myMethod(){
+        System.out.println("You have called me! My name is: myMethod");
+    }
+}
